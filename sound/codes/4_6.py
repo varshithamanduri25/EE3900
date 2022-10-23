d225d80e3e09a382e7d14c183a8ca8881df848f2
varshithamanduri25 <111949913+varshithamanduri25@users.noreply.github.com>
2022-10-23 22:10:55 +0530

    Add files via upload

---
+++ sound/codes/4_6.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+#DTFT
+def H(z):
+	num = np.polyval([1,0,1],z**(-1))
+	den = np.polyval([0.5,1],z**(-1))
+	H = num/den
+	return H
+		
+
+
+#Input and Output
+omega = np.linspace(-3*np.pi,3*np.pi,1e2)
+
+#subplots
+plt.plot(omega, abs(H(np.exp(1j*omega))))
+plt.title('Filter Frequency Response')
+plt.xlabel('$\omega$')
+plt.ylabel('$|H(e^{\jmath\omega})| $')
+plt.grid()
+
+
+plt.show()
